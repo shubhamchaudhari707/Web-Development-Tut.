7f9e6f271399dea269f14aa080f2a8b50b57953c
shubham <shubhamchaudhari707@gmail.com>
2021-03-31 07:58:25 +0530

    web development tut

---
+++ javascript6.js
@@ -0,0 +1,36 @@
+console.log("This is tutoral is 53");
+
+// let name1="harry"
+// let name2="shubham"
+// let name3="roshan"
+// let name4="sagar"
+// console.log(name1+" is a good boy");
+// console.log(name2+" is a good boy");
+// console.log(name3+" is a good boy");
+// console.log(name4+" is a good boy");
+
+// shurucut method function using
+
+function greet(name, greettext) {
+    let name1 = "tushar"
+    console.log(greettext + " " + name);
+    console.log(name + " is a good boy");
+}
+
+
+let name1="harry"
+let name2 = "shubham"
+let name3 = "roshan"
+let name4 = "sagar"
+let greettext = "good morning"
+greet(name1, greettext)
+greet(name2, greettext)
+greet(name3, greettext)
+greet(name4, greettext)
+
+
+
+
+
+
+
